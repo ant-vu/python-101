c2ca1c56bd51be15bc426e257a540ce8b2dc26fe
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-05-28 22:38:55 -0400

    feat: add solution

--- leetcode/9.py
+++ leetcode/9.py
@@ -1,12 +1,21 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         # time: O(n), space: O(1)
-        str_x = str(x)
-        left = 0
-        right = len(str_x) - 1
-        while left < right:
-            if str_x[left] != str_x[right]:
-                return False
-            left += 1
-            right -= 1
-        return True
+        if x < 0 or (x != 0 and x % 10 == 0):
+            return False
+        half = 0
+        while x > half:
+            half = (half * 10) + (x % 10)
+            x //= 10
+        return x == half or x == half // 10
+
+        # time: O(n), space: O(1)
+        # str_x = str(x)
+        # left = 0
+        # right = len(str_x) - 1
+        # while left < right:
+        #     if str_x[left] != str_x[right]:
+        #         return False
+        #     left += 1
+        #     right -= 1
+        # return True
