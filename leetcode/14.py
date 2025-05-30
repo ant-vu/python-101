08bce0cbbcd3de3296ca35138f111eee15f19a4e
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-05-30 19:57:11 -0400

    feat: add solution

--- leetcode/14.py
+++ leetcode/14.py
@@ -1,17 +1,29 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         # time: O(n^2), space: O(1)
-        if len(strs) == 1:
-            return strs[0]
+        if len(strs) == 0:
+            return ""
+        
+        prefix = strs[0]
+        for i in range(1, len(strs)):
+            while strs[i].find(prefix) != 0:
+                prefix = prefix[:len(prefix) - 1]
+                if prefix == "":
+                    return ""
+        return prefix
 
-        shortest = min(strs, key=len)
-        res = 0
-        for length in range(len(shortest) + 1):
-            flag = True
-            for s in strs:
-                if s[:length] != shortest[:length]:
-                    flag = False
-                    break
-            if flag:
-                res = max(res, length)
-        return shortest[:res]
+        # time: O(n^2), space: O(1)
+        # if len(strs) == 1:
+        #     return strs[0]
+
+        # shortest = min(strs, key=len)
+        # res = 0
+        # for length in range(len(shortest) + 1):
+        #     flag = True
+        #     for s in strs:
+        #         if s[:length] != shortest[:length]:
+        #             flag = False
+        #             break
+        #     if flag:
+        #         res = max(res, length)
+        # return shortest[:res]
