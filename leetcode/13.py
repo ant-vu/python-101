3b17c42e24b073f91b2be8858d98cfb34471fa24
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-05-29 20:25:12 -0400

    feat: add solution

--- leetcode/13.py
+++ leetcode/13.py
@@ -2,30 +2,46 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         # time: O(n), space: O(1)
         mapping = {'I': 1,
-                   'IV': 4,
                    'V': 5,
-                   'IX': 9,
                    'X': 10,
-                   'XL': 40,
                    'L': 50,
-                   'XC': 90,
                    'C': 100,
-                   'CD': 400,
                    'D': 500,
-                   'CM': 900,
                    'M': 1000}
-        idx = 0
         res = 0
-        while idx < len(s) - 1:
-            if s[idx : idx + 2] in ['IV', 'IX', 'XL', 'XC', 'CD', 'CM']:
-                res += mapping[s[idx : idx + 2]]
-                idx += 2
+        for a, b in zip(s, s[1 :]):
+            if mapping[a] < mapping[b]:
+                res -= mapping[a]
             else:
-                res += mapping[s[idx]]
-                idx += 1
-        if s[-2 :] not in ['IV', 'IX', 'XL', 'XC', 'CD', 'CM']:
-            res += mapping[s[-1]]
-        return res
+                res += mapping[a]
+        return res + mapping[s[-1]]
+
+        # time: O(n), space: O(1)
+        # mapping = {'I': 1,
+        #            'IV': 4,
+        #            'V': 5,
+        #            'IX': 9,
+        #            'X': 10,
+        #            'XL': 40,
+        #            'L': 50,
+        #            'XC': 90,
+        #            'C': 100,
+        #            'CD': 400,
+        #            'D': 500,
+        #            'CM': 900,
+        #            'M': 1000}
+        # idx = 0
+        # res = 0
+        # while idx < len(s) - 1:
+        #     if s[idx : idx + 2] in ['IV', 'IX', 'XL', 'XC', 'CD', 'CM']:
+        #         res += mapping[s[idx : idx + 2]]
+        #         idx += 2
+        #     else:
+        #         res += mapping[s[idx]]
+        #         idx += 1
+        # if s[-2 :] not in ['IV', 'IX', 'XL', 'XC', 'CD', 'CM']:
+        #     res += mapping[s[-1]]
+        # return res
 
         # time: O(n), space: O(n)
         # nums = []
