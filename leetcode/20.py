8ec3126ca6a54922b06c36c7651893664b87406d
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-05-31 11:03:10 -0400

    feat: add solution

--- leetcode/20.py
+++ leetcode/20.py
@@ -1,11 +1,26 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         # time: O(n), space: O(n)
+        def is_pair(last, cur):
+            if last == "(" and cur == ")" or last == "{" and cur == "}" or last == "[" and cur == "]":
+                return True
+            return False
+
         stack = []
-        mapping = {')': '(', '}': '{', ']': '['}
-        for char in s:
-            if char not in mapping:
-                stack.append(char)
-            elif not stack or stack.pop() != mapping[char]:
-                return False
+        for i in range(len(s)):
+            if stack:
+                if is_pair(stack[-1], s[i]):
+                    stack.pop()
+                    continue
+            stack.append(s[i])
         return not stack
+
+        # time: O(n), space: O(n)
+        # stack = []
+        # mapping = {')': '(', '}': '{', ']': '['}
+        # for char in s:
+        #     if char not in mapping:
+        #         stack.append(char)
+        #     elif not stack or stack.pop() != mapping[char]:
+        #         return False
+        # return not stack
