293d2e38493570dbc71b5c1011642813856f392d
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-05-30 20:18:40 -0400

    feat: add solution

--- leetcode/14.py
+++ leetcode/14.py
@@ -1,23 +1,42 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        # time: O(n^2), space: O(nlogn)
-        def LCP(left, right):
-            min_len = min(len(left), len(right))
-            for i in range(min_len):
-                if left[i] != right[i]:
-                    return left[:i]
-            return left[:min_len]
-        
-        def divide_and_conquer(strs, left, right):
-            if left == right:
-                return strs[left]
+        # time: O(n^2logn), space: O(1)
+        def is_common_prefix(strs, length):
+            str1 = strs[0][:length]
+            for i in range(1, len(strs)):
+                if not strs[i].startswith(str1):
+                    return False
+            return True
+
+        min_len = min(len(x) for x in strs)
+        low = 1
+        high = min_len
+        while low <= high:
+            mid = low + (high - low) // 2
+            if is_common_prefix(strs, mid):
+                low = mid + 1
             else:
-                mid = left + (right - left) // 2
-                lcp_left = divide_and_conquer(strs, left, mid)
-                lcp_right = divide_and_conquer(strs, mid + 1, right)
-                return LCP(lcp_left, lcp_right)
+                high = mid - 1
+        return strs[0][:(low + high) // 2]
+
+        # time: O(n^2), space: O(nlogn)
+        # def LCP(left, right):
+        #     min_len = min(len(left), len(right))
+        #     for i in range(min_len):
+        #         if left[i] != right[i]:
+        #             return left[:i]
+        #     return left[:min_len]
         
-        return divide_and_conquer(strs, 0, len(strs) - 1)
+        # def divide_and_conquer(strs, left, right):
+        #     if left == right:
+        #         return strs[left]
+        #     else:
+        #         mid = left + (right - left) // 2
+        #         lcp_left = divide_and_conquer(strs, left, mid)
+        #         lcp_right = divide_and_conquer(strs, mid + 1, right)
+        #         return LCP(lcp_left, lcp_right)
+        
+        # return divide_and_conquer(strs, 0, len(strs) - 1)
 
         # time: O(n^2), space: O(1)
         # for i in range(len(strs[0])):
