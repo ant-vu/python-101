277b6d73a66d49e9b928d9763ef51efc3f2a67af
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-01 17:14:46 -0400

    feat: add solution

--- leetcode/21.py
+++ leetcode/21.py
@@ -5,19 +5,27 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # time: O(n), space: O(n)
+        if not list1 or not list2:
+            return list1 or list2
+        elif list1.val < list2.val:
+            list1, list2 = list2, list1
+        list2.next = self.mergeTwoLists(list2.next, list1)
+        return list2
+
         # time: O(n), space: O(1)
-        dummy = ListNode()
-        head = dummy
-        while list1 and list2:
-            if list1.val < list2.val:
-                head.next = list1
-                list1 = list1.next
-            else:
-                head.next = list2
-                list2 = list2.next
-            head = head.next
-        if list1:
-            head.next = list1
-        elif list2:
-            head.next = list2
-        return dummy.next
+        # dummy = ListNode()
+        # head = dummy
+        # while list1 and list2:
+        #     if list1.val < list2.val:
+        #         head.next = list1
+        #         list1 = list1.next
+        #     else:
+        #         head.next = list2
+        #         list2 = list2.next
+        #     head = head.next
+        # if list1:
+        #     head.next = list1
+        # elif list2:
+        #     head.next = list2
+        # return dummy.next
