dc1799ff9b01eed08259b932361a217ad705a581
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-09 10:18:34 -0400

    feat: add solution

--- leetcode/58.py
+++ leetcode/58.py
@@ -1,13 +1,22 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         # time: O(n), space: O(1)
-        l = None
-        r = None
-        for i in range(len(s) - 1, -1, -1):
-            if s[i].isalpha() and not r:
-                r = i
-            elif not s[i].isalpha() and r and not l:
-                l = i
-        if l == None:
-            l = -1
-        return r - l
+        end = len(s) - 1
+        while end >= 0 and s[end] == " ":
+            end -= 1
+        start = end
+        while start >= 0 and s[start] != " ":
+            start -= 1
+        return end - start
+
+        # time: O(n), space: O(1)
+        # l = None
+        # r = None
+        # for i in range(len(s) - 1, -1, -1):
+        #     if s[i].isalpha() and not r:
+        #         r = i
+        #     elif not s[i].isalpha() and r and not l:
+        #         l = i
+        # if l == None:
+        #     l = -1
+        # return r - l
