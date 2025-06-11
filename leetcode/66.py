f8c8711561381135656e7755bb61a55e94fcb9e7
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-10 20:20:31 -0400

    feat: add solution

--- leetcode/66.py
+++ leetcode/66.py
@@ -1,11 +1,23 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        # time: O(n), space: O(n)
+        # time: O(n), space: O(1)
         digits = digits[::-1]
-        total = 1
-        for idx in range(len(digits)):
-            total += digits[idx] * (10 ** idx)
-        res = []
-        for num in str(total):
-            res.append(int(num))
-        return res
+        for i in range(len(digits)):
+            digits[i] += 1
+            if digits[i] == 10:
+                digits[i] = 0
+                if i == len(digits) - 1:
+                    digits.append(1)
+            else:
+                break
+        return digits[::-1]
+
+        # time: O(n), space: O(n)
+        # digits = digits[::-1]
+        # total = 1
+        # for idx in range(len(digits)):
+        #     total += digits[idx] * (10 ** idx)
+        # res = []
+        # for num in str(total):
+        #     res.append(int(num))
+        # return res
