06b74184bd55ab9a8eb19cef56bf6ab062cd54e8
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-11 09:45:05 -0400

    feat: add solution

--- leetcode/67.py
+++ leetcode/67.py
@@ -1,23 +1,26 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        # time: O(m * n), space: O(m + n)
-        while len(a) < len(b):
-            a = "0" + a
-        while len(a) > len(b):
-            b = "0" + b
-        res = ""
-        carry = 0
-        for i in range(len(a) - 1, -1, -1):
-            if a[i] == "1" and b[i] == "1" and carry:
-                res = "1" + res
-                carry = 1
-            elif (a[i] == "1" and b[i] == "1" and not carry) or (a[i] == "1" and b[i] == "0" and carry) or (a[i] == "0" and b[i] == "1" and carry):
-                res = "0" + res
-                carry = 1
-            elif (a[i] == "1" and b[i] == "0" and not carry) or (a[i] == "0" and b[i] == "1" and not carry) or (a[i] == "0" and b[i] == "0" and carry):
-                res = "1" + res
-                carry = 0
-            else:
-                res = "0" + res
-                carry = 0
-        return "1" + res if carry else res
+        # time: O(max(m, n)), space: O(max(m, n))
+        return bin(int(a, 2) + int(b, 2))[2:]
+
+        # time: O(max(m, n)^2), space: O(max(m, n))
+        # while len(a) < len(b):
+        #     a = "0" + a
+        # while len(a) > len(b):
+        #     b = "0" + b
+        # res = ""
+        # carry = 0
+        # for i in range(len(a) - 1, -1, -1):
+        #     if a[i] == "1" and b[i] == "1" and carry:
+        #         res = "1" + res
+        #         carry = 1
+        #     elif (a[i] == "1" and b[i] == "1" and not carry) or (a[i] == "1" and b[i] == "0" and carry) or (a[i] == "0" and b[i] == "1" and carry):
+        #         res = "0" + res
+        #         carry = 1
+        #     elif (a[i] == "1" and b[i] == "0" and not carry) or (a[i] == "0" and b[i] == "1" and not carry) or (a[i] == "0" and b[i] == "0" and carry):
+        #         res = "1" + res
+        #         carry = 0
+        #     else:
+        #         res = "0" + res
+        #         carry = 0
+        # return "1" + res if carry else res
