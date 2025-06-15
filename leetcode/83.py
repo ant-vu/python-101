612f906a13b42dc0faf15cf886c2b4eb8fc4ede5
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-14 22:27:52 -0400

    feat: add solution

--- leetcode/83.py
+++ leetcode/83.py
@@ -6,18 +6,27 @@
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         # time: O(n), space: O(1)
-        dummy = ListNode(next=head)
-        if not head:
-            return None
-        slow = head
-        fast = head.next
-        while fast:
-            if slow.val == fast.val:
-                if fast.next == None:
-                    slow.next = None
-                fast = fast.next
-                continue
-            slow.next = fast
-            slow = fast
-            fast = fast.next
-        return dummy.next
+        res = head
+        while head and head.next:
+            if head.val == head.next.val:
+                head.next = head.next.next
+            else:
+                head = head.next
+        return res
+
+        # time: O(n), space: O(1)
+        # dummy = ListNode(next=head)
+        # if not head:
+        #     return None
+        # slow = head
+        # fast = head.next
+        # while fast:
+        #     if slow.val == fast.val:
+        #         if fast.next == None:
+        #             slow.next = None
+        #         fast = fast.next
+        #         continue
+        #     slow.next = fast
+        #     slow = fast
+        #     fast = fast.next
+        # return dummy.next
