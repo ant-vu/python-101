556b32b384e5b71bf1cd5603e528f24acdf9337c
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-14 22:24:52 -0400

    feat: add solution

---
+++ leetcode/83.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # time: O(n), space: O(1)
+        dummy = ListNode(next=head)
+        if not head:
+            return None
+        slow = head
+        fast = head.next
+        while fast:
+            if slow.val == fast.val:
+                if fast.next == None:
+                    slow.next = None
+                fast = fast.next
+                continue
+            slow.next = fast
+            slow = fast
+            fast = fast.next
+        return dummy.next
