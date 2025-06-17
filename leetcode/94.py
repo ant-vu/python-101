07417d40313402fc142ad01f389923394bc7b69e
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-17 12:13:51 -0400

    feat: add solution

--- leetcode/94.py
+++ leetcode/94.py
@@ -8,11 +8,24 @@ class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         # time: O(n), space: O(n)
         res = []
-        def helper(node):
-            if not node:
-                return
-            helper(node.left)
-            res.append(node.val)
-            helper(node.right)
-        helper(root)
+        stack = []
+        curr = root
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            res.append(curr.val)
+            curr = curr.right
         return res
+
+        # time: O(n), space: O(n)
+        # res = []
+        # def helper(node):
+        #     if not node:
+        #         return
+        #     helper(node.left)
+        #     res.append(node.val)
+        #     helper(node.right)
+        # helper(root)
+        # return res
