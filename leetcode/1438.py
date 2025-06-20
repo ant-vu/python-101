de26f0b731edb8a7a7e7cede8dce5c88965afdb4
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-19 23:03:55 -0400

    feat: add solution

--- leetcode/1438.py
+++ leetcode/1438.py
@@ -1,18 +1,39 @@
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        # time: O(nlogn), space: O(n)
-        window = SortedDict()
+        # time: O(n), space: O(n)
+        max_deque = deque()
+        min_deque = deque()
         left = 0
         max_length = 0
         for right in range(len(nums)):
-            window[nums[right]] = window.get(nums[right], 0) + 1
-            while window.peekitem(-1)[0] - window.peekitem(0)[0] > limit:
-                window[nums[left]] -= 1
-                if window[nums[left]] == 0:
-                    window.pop(nums[left])
+            while max_deque and max_deque[-1] < nums[right]:
+                max_deque.pop()
+            max_deque.append(nums[right])
+            while min_deque and min_deque[-1] > nums[right]:
+                min_deque.pop()
+            min_deque.append(nums[right])
+            while max_deque[0] - min_deque[0] > limit:
+                if max_deque[0] == nums[left]:
+                    max_deque.popleft()
+                if min_deque[0] == nums[left]:
+                    min_deque.popleft()
                 left += 1
             max_length = max(max_length, right - left + 1)
         return max_length
+
+        # time: O(nlogn), space: O(n)
+        # window = SortedDict()
+        # left = 0
+        # max_length = 0
+        # for right in range(len(nums)):
+        #     window[nums[right]] = window.get(nums[right], 0) + 1
+        #     while window.peekitem(-1)[0] - window.peekitem(0)[0] > limit:
+        #         window[nums[left]] -= 1
+        #         if window[nums[left]] == 0:
+        #             window.pop(nums[left])
+        #         left += 1
+        #     max_length = max(max_length, right - left + 1)
+        # return max_length
 
         # time: O(nlogn), space: O(n)
         # max_heap = []
