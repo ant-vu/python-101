a7adafba4a6429496f1ffb50dc3d03bc61f41584
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-20 18:19:26 -0400

    feat: add solution

--- 485.py
+++ 485.py
@@ -2,9 +2,20 @@ class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
         # time: O(n), space: O(1)
         res = 0
-        l = 0
-        for r in range(len(nums)):
-            if nums[r] != 1:
-                l = r + 1
-            res = max(res, r - l + 1)
+        cur = 0
+        for n in nums:
+            if n == 1:
+                cur += 1
+                res = max(res, cur)
+            else:
+                cur = 0
         return res
+
+        # time: O(n), space: O(1)
+        # res = 0
+        # l = 0
+        # for r in range(len(nums)):
+        #     if nums[r] != 1:
+        #         l = r + 1
+        #     res = max(res, r - l + 1)
+        # return res
