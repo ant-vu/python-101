328c82b1fa58522d9e17782fd348f4f09b21879b
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-20 10:10:29 -0400

    feat: add solution

--- leetcode/1768.py
+++ leetcode/1768.py
@@ -2,10 +2,19 @@ class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         # time: O(m+n), space: O(m+n)
         res = []
-        length = min(len(word1), len(word2))
-        for i in range(length):
-            res.append(word1[i])
-            res.append(word2[i])
-        res.extend(word1[length:])
-        res.extend(word2[length:])
+        for i in range(max(len(word1), len(word2))):
+            if i < len(word1):
+                res += word1[i]
+            if i < len(word2):
+                res += word2[i]
         return "".join(res)
+
+        # time: O(m+n), space: O(m+n)
+        # res = []
+        # length = min(len(word1), len(word2))
+        # for i in range(length):
+        #     res.append(word1[i])
+        #     res.append(word2[i])
+        # res.extend(word1[length:])
+        # res.extend(word2[length:])
+        # return "".join(res)
