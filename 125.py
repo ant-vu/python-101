ffcd6340e14fc6378538d0719e196313fd1432da
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-20 19:45:00 -0400

    feat: add solution

--- 125.py
+++ 125.py
@@ -1,12 +1,7 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         # time: O(n), space: O(n)
-        clean = ""
-        for c in s:
-            if c.isalnum():
-                clean += c
-        clean = clean.lower()
-        print(clean)
+        clean = re.sub("[^a-zA-Z0-9]", "", s).lower()
         l, r = 0, len(clean) - 1
         while l < r:
             if clean[l] != clean[r]:
@@ -14,3 +9,13 @@ class Solution:
             l += 1
             r -= 1
         return True
+
+        # time: O(n), space: O(n)
+        # clean = "".join(c.lower() for c in s if c.isalnum())
+        # l, r = 0, len(clean) - 1
+        # while l < r:
+        #     if clean[l] != clean[r]:
+        #         return False
+        #     l += 1
+        #     r -= 1
+        # return True
