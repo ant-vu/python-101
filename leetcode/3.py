3452698bb95f75ab58c02266baea437c60b8d208
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-19 21:30:19 -0400

    feat: add solution

--- leetcode/3.py
+++ leetcode/3.py
@@ -1,13 +1,25 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         # time: O(n), space: O(1)
-        unique = set()
+        count = {}
         res = 0
         l = 0
-        for r in range(len(s)):
-            while s[r] in unique:
-                unique.remove(s[l])
+        for r, c in enumerate(s):
+            count[c] = count.get(c, 0) + 1
+            while count[c] > 1:
+                count[s[l]] -= 1
                 l += 1
-            unique.add(s[r])
             res = max(r - l + 1, res)
         return res
+
+        # time: O(n), space: O(1)
+        # unique = set()
+        # res = 0
+        # l = 0
+        # for r in range(len(s)):
+        #     while s[r] in unique:
+        #         unique.remove(s[l])
+        #         l += 1
+        #     unique.add(s[r])
+        #     res = max(r - l + 1, res)
+        # return res
