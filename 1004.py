c45365fe904f973fd71113a718a4e8b2309a135a
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-20 19:05:44 -0400

    feat: add solution

--- 1004.py
+++ 1004.py
@@ -1,16 +1,27 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         # time: O(n), space: O(1)
-        res = 0
-        zeros = 0
         l = 0
         for r in range(len(nums)):
             if nums[r] == 0:
-                zeros += 1
-            if nums[r] == 1 or zeros <= k:
-                res = max(res, r - l + 1)
-            while zeros > k:
+                k -= 1
+            if k < 0:
                 if nums[l] == 0:
-                    zeros -= 1
+                    k += 1
                 l += 1
-        return res
+        return r - l + 1
+
+        # time: O(n), space: O(1)
+        # res = 0
+        # zeros = 0
+        # l = 0
+        # for r in range(len(nums)):
+        #     if nums[r] == 0:
+        #         zeros += 1
+        #     if nums[r] == 1 or zeros <= k:
+        #         res = max(res, r - l + 1)
+        #     while zeros > k:
+        #         if nums[l] == 0:
+        #             zeros -= 1
+        #         l += 1
+        # return res
