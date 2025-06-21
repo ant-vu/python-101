6d70e92c356f9e7604a42f040ab8272109a3ae52
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-21 10:17:39 -0400

    feat: add solution

--- 198.py
+++ 198.py
@@ -1,13 +1,23 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        # time: O(n), space: O(1)
+        if len(nums) == 0:
+            return 0
+        memo = [-1] * (len(nums) + 1)
+        memo[0] = 0
+        memo[1] = nums[0]
+        for i in range(1, len(nums)):
+            memo[i + 1] = max(memo[i], memo[i - 1] + nums[i])
+        return memo[len(nums)]
+
         # time: O(n), space: O(n)
-        memo = [-1] * len(nums)
-        def helper(nums, i):
-            if i < 0:
-                return 0
-            elif memo[i] >= 0:
-                return memo[i]
-            res = max(helper(nums, i - 2) + nums[i], helper(nums, i - 1))
-            memo[i] = res
-            return res
-        return helper(nums, len(nums) - 1)
+        # memo = [-1] * len(nums)
+        # def helper(nums, i):
+        #     if i < 0:
+        #         return 0
+        #     elif memo[i] >= 0:
+        #         return memo[i]
+        #     res = max(helper(nums, i - 2) + nums[i], helper(nums, i - 1))
+        #     memo[i] = res
+        #     return res
+        # return helper(nums, len(nums) - 1)
