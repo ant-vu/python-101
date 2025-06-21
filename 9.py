1e8f27739370768f0a75913b4248cc7a6dcc62e2
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-21 15:44:54 -0400

    feat: add solution

--- 9.py
+++ 9.py
@@ -1,13 +1,19 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        # time: O(n), space: O(1)
-        if x < 0 or (x != 0 and x % 10 == 0):
-            return False
-        half = 0
-        while x > half:
-            half = (half * 10) + (x % 10)
-            x //= 10
-        return x == half or x == half // 10
+        # time: O(logn), space: O(1)
+        reverse, tmp = 0, x
+        while tmp > 0:
+            reverse, tmp = (reverse * 10) + (tmp % 10), tmp // 10
+        return x == reverse
+
+        # time: O(logn), space: O(1)
+        # if x < 0 or (x != 0 and x % 10 == 0):
+        #     return False
+        # half = 0
+        # while x > half:
+        #     half = (half * 10) + (x % 10)
+        #     x //= 10
+        # return x == half or x == half // 10
 
         # time: O(n), space: O(1)
         # str_x = str(x)
