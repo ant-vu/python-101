d199f5e55de189cac166aaf2e50582bad09f7583
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-21 16:33:57 -0400

    feat: add solution

--- 387.py
+++ 387.py
@@ -1,10 +1,17 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        # time: O(n), space: O(n)
-        counter = defaultdict(int)
-        for c in s:
-            counter[c] += 1
+        # time: O(n), space: O(1)
+        counter = Counter(s)
         for i, c in enumerate(s):
             if counter[c] == 1:
                 return i
         return -1
+
+        # time: O(n), space: O(1)
+        # counter = defaultdict(int)
+        # for c in s:
+        #     counter[c] += 1
+        # for i, c in enumerate(s):
+        #     if counter[c] == 1:
+        #         return i
+        # return -1
