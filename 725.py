93ffc42490f4193bc795387b0046f1b32d2cb813
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-20 22:04:14 -0400

    feat: add solution

--- 725.py
+++ 725.py
@@ -5,7 +5,7 @@
 #         self.next = next
 class Solution:
     def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
-        # time: O(n), space: O(n)
+        # time: O(n), space: O(1)
         ans = [None] * k
         size = 0
         current = head
@@ -14,20 +14,47 @@ class Solution:
             current = current.next
         split_size = size // k
         num_remaining_parts = size % k
-        current = head
+        current = prev = head
         for i in range(k):
-            new_part = ListNode(0)
-            tail = new_part
+            new_part = current
             current_size = split_size
             if num_remaining_parts > 0:
                 num_remaining_parts -= 1
                 current_size += 1
-            for j in range(current_size):
-                tail.next = ListNode(current.val)
-                tail = tail.next
-                current = current.next
-            ans[i] = new_part.next
+            j = 0
+            while j < current_size:
+                prev = current
+                if current:
+                    current = current.next
+                j += 1
+            if prev:
+                prev.next = None
+            ans[i] = new_part
         return ans
+
+        # time: O(n), space: O(n)
+        # ans = [None] * k
+        # size = 0
+        # current = head
+        # while current is not None:
+        #     size += 1
+        #     current = current.next
+        # split_size = size // k
+        # num_remaining_parts = size % k
+        # current = head
+        # for i in range(k):
+        #     new_part = ListNode(0)
+        #     tail = new_part
+        #     current_size = split_size
+        #     if num_remaining_parts > 0:
+        #         num_remaining_parts -= 1
+        #         current_size += 1
+        #     for j in range(current_size):
+        #         tail.next = ListNode(current.val)
+        #         tail = tail.next
+        #         current = current.next
+        #     ans[i] = new_part.next
+        # return ans
 
         # time: O(n), space: O(n)
         # length = 0
