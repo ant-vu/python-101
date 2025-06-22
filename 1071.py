9dfc73cf63f478370c191d96b1c85ca2b0612104
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-21 22:35:13 -0400

    feat: add solution

---
+++ 1071.py
@@ -0,0 +1,10 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        # time: O(min(m, n) * (m + n)), space: O(min(m, n))
+        res = ""
+        gcd = ""
+        for i in range(min(len(str1), len(str2))):
+            gcd += str1[i]
+            if (len(str1) % len(gcd) == 0 and gcd * (len(str1) // len(gcd)) == str1) and (len(str2) % len(gcd) == 0 and gcd * (len(str2) // len(gcd)) == str2):
+                res = gcd
+        return res
