557a17e136b9c5f5339a6cb30f546b7091e6ef17
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-23 16:06:45 -0400

    feat: add solution

--- 392.py
+++ 392.py
@@ -1,12 +1,20 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         # time: O(n), space: O(1)
-        cur = 0
-        if cur == len(s):
-            return True
-        for i in range(len(t)):
-            if s[cur] == t[i]:
-                cur += 1
-            if cur == len(s):
-                return True
-        return False
+        sp = tp = 0
+        while sp < len(s) and tp < len(t):
+            if s[sp] == t[tp]:
+                sp += 1
+            tp += 1
+        return sp == len(s)
+
+        # time: O(n), space: O(1)
+        # cur = 0
+        # if cur == len(s):
+        #     return True
+        # for i in range(len(t)):
+        #     if s[cur] == t[i]:
+        #         cur += 1
+        #     if cur == len(s):
+        #         return True
+        # return False
