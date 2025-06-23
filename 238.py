f1a5f5b2826378a0db2b748f65763f0b2ae254e6
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-23 13:41:55 -0400

    feat: add solution

--- 238.py
+++ 238.py
@@ -1,22 +1,35 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # time: O(n), space: O(n)
-        products = []
+        # time: O(n), space: O(1)
+        n = len(nums)
+        res = [1] * n
         cur = 1
-        for n in nums:
-            cur *= n
-            products.append(cur)
-        products_rev = []
+        for i in range(len(nums)):
+            res[i] *= cur
+            cur *= nums[i]
         cur = 1
         for i in range(len(nums) - 1, -1, -1):
+            res[i] *= cur
             cur *= nums[i]
-            products_rev.insert(0, cur)
-        res = []
-        for i in range(len(nums)):
-            if i == 0:
-                res.append(products_rev[1])
-            elif i == len(nums) - 1:
-                res.append(products[-2])
-            else:
-                res.append(products[i - 1] * products_rev[i + 1])
         return res
+
+        # time: O(n), space: O(n)
+        # products = []
+        # cur = 1
+        # for n in nums:
+        #     cur *= n
+        #     products.append(cur)
+        # products_rev = []
+        # cur = 1
+        # for i in range(len(nums) - 1, -1, -1):
+        #     cur *= nums[i]
+        #     products_rev.insert(0, cur)
+        # res = []
+        # for i in range(len(nums)):
+        #     if i == 0:
+        #         res.append(products_rev[1])
+        #     elif i == len(nums) - 1:
+        #         res.append(products[-2])
+        #     else:
+        #         res.append(products[i - 1] * products_rev[i + 1])
+        # return res
