cb7a3ca37af4fb9a1217b3fe83a8372329ad1369
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-23 13:35:29 -0400

    feat: add solution

---
+++ 238.py
@@ -0,0 +1,22 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # time: O(n), space: O(n)
+        products = []
+        cur = 1
+        for n in nums:
+            cur *= n
+            products.append(cur)
+        products_rev = []
+        cur = 1
+        for i in range(len(nums) - 1, -1, -1):
+            cur *= nums[i]
+            products_rev.insert(0, cur)
+        res = []
+        for i in range(len(nums)):
+            if i == 0:
+                res.append(products_rev[1])
+            elif i == len(nums) - 1:
+                res.append(products[-2])
+            else:
+                res.append(products[i - 1] * products_rev[i + 1])
+        return res
