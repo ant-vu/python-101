b5ed80a0c546f43b8567f6d99aa6ff5c694136b6
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-23 21:41:13 -0400

    feat: add solution

---
+++ 724.py
@@ -0,0 +1,18 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        # time: O(n), space: O(n)
+        if len(nums) == 1:
+            return 0
+        forward, backward = nums[:], nums[:]
+        for i in range(len(nums) - 1):
+            forward[i + 1] += forward[i]
+        for i in range(len(nums) - 1, 0, -1):
+            backward[i - 1] += backward[i]
+        for i in range(len(nums)):
+            if i == 0 and backward[1] == 0:
+                return 0
+            elif i == len(nums) - 1 and forward[-2] == 0:
+                return len(nums) - 1
+            elif 0 < i < len(nums) - 1 and forward[i - 1] == backward[i + 1]:
+                return i
+        return -1
