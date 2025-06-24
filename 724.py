6e2618b6f4e6a43f703db2245d9b89d856351889
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-23 21:43:34 -0400

    feat: add solution

--- 724.py
+++ 724.py
@@ -1,18 +1,27 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        # time: O(n), space: O(n)
-        if len(nums) == 1:
-            return 0
-        forward, backward = nums[:], nums[:]
-        for i in range(len(nums) - 1):
-            forward[i + 1] += forward[i]
-        for i in range(len(nums) - 1, 0, -1):
-            backward[i - 1] += backward[i]
-        for i in range(len(nums)):
-            if i == 0 and backward[1] == 0:
-                return 0
-            elif i == len(nums) - 1 and forward[-2] == 0:
-                return len(nums) - 1
-            elif 0 < i < len(nums) - 1 and forward[i - 1] == backward[i + 1]:
+        # time: O(n), space: O(1)
+        l, r = 0, sum(nums)
+        for i, n in enumerate(nums):
+            r -= n
+            if l == r:
                 return i
+            l += n
         return -1
+
+        # time: O(n), space: O(n)
+        # if len(nums) == 1:
+        #     return 0
+        # forward, backward = nums[:], nums[:]
+        # for i in range(len(nums) - 1):
+        #     forward[i + 1] += forward[i]
+        # for i in range(len(nums) - 1, 0, -1):
+        #     backward[i - 1] += backward[i]
+        # for i in range(len(nums)):
+        #     if i == 0 and backward[1] == 0:
+        #         return 0
+        #     elif i == len(nums) - 1 and forward[-2] == 0:
+        #         return len(nums) - 1
+        #     elif 0 < i < len(nums) - 1 and forward[i - 1] == backward[i + 1]:
+        #         return i
+        # return -1
