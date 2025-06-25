342dbbd459c2a850693133afc6dbd85b2572e907
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-25 17:10:48 -0400

    feat: add solution

---
+++ 872.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        leaves1, leaves2 = [], []
+        def dfs(node, li):
+            if not node:
+                return None
+            if not node.left and not node.right:
+                li.append(node.val)
+            dfs(node.left, li)
+            dfs(node.right, li)
+        dfs(root1, leaves1)
+        dfs(root2, leaves2)
+        return leaves1 == leaves2
