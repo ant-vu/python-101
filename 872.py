5dad155e8d4ec275197106a76b469f2b712e37f4
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-25 17:17:20 -0400

    feat: add solution

--- 872.py
+++ 872.py
@@ -6,14 +6,24 @@
 #         self.right = right
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        leaves1, leaves2 = [], []
-        def dfs(node, li):
-            if not node:
-                return None
-            if not node.left and not node.right:
-                li.append(node.val)
-            dfs(node.left, li)
-            dfs(node.right, li)
-        dfs(root1, leaves1)
-        dfs(root2, leaves2)
-        return leaves1 == leaves2
+        # time: O(m + n), space: O(m + n)
+        def dfs(node):
+            if node:
+                if not node.left and not node.right:
+                    yield node.val
+                yield from dfs(node.left)
+                yield from dfs(node.right)
+        return list(dfs(root1)) == list(dfs(root2))
+
+        # time: O(m + n), space: O(m + n)
+        # leaves1, leaves2 = [], []
+        # def dfs(node, li):
+        #     if not node:
+        #         return None
+        #     if not node.left and not node.right:
+        #         li.append(node.val)
+        #     dfs(node.left, li)
+        #     dfs(node.right, li)
+        # dfs(root1, leaves1)
+        # dfs(root2, leaves2)
+        # return leaves1 == leaves2
