7b7c7d9d1b6431c4ed90aafd46fb4759180f89a0
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-26 19:08:57 -0400

    feat: add solution

---
+++ 199.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        # time: O(n), space: O(n)
+        if not root:
+            return []
+        res = []
+        q = deque()
+        q.append(root)
+        while q:
+            tmp = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                tmp.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            res.append(tmp[-1])
+        return res
