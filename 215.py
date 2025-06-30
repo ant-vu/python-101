8107dba5a576fa32454389b88658c360878d763a
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-06-30 09:11:59 -0400

    feat: add solution

---
+++ 215.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        # time: O(nlogn), space: O(n)
+        nums = [-n for n in nums]
+        heapq.heapify(nums)
+        for i in range(k - 1):
+            heapq.heappop(nums)
+        return -heapq.heappop(nums)
