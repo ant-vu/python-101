dd4bc51690afc52a0d60bca77d1a38e7b7b4f5a5
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-01 11:29:09 -0400

    feat: add solution

--- 746.py
+++ 746.py
@@ -1,10 +1,16 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         # time: O(n), space: O(1)
-        dp = [0] * (len(cost) + 1)
-        for i in range(len(cost) - 1, -1, -1):
-            if i == len(cost) - 1:
-                dp[i] = cost[i]
-            else:
-                dp[i] = cost[i] + min(dp[i + 1], dp[i + 2])
-        return min(dp[0], dp[1])
+        cost.append(0)
+        for i in range(len(cost) - 4, -1, -1):
+            cost[i] += min(cost[i + 1], cost[i + 2])
+        return min(cost[0], cost[1])
+
+        # time: O(n), space: O(1)
+        # dp = [0] * (len(cost) + 1)
+        # for i in range(len(cost) - 1, -1, -1):
+        #     if i == len(cost) - 1:
+        #         dp[i] = cost[i]
+        #     else:
+        #         dp[i] = cost[i] + min(dp[i + 1], dp[i + 2])
+        # return min(dp[0], dp[1])
