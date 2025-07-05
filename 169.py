bb1d7bcda206c0ef16c9627e3863fa7b02bb5faa
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-05 17:27:22 -0400

    feat: add solution

--- 169.py
+++ 169.py
@@ -2,9 +2,17 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         # time: O(n), space: O(n)
         cnt = {}
+        majority = len(nums) // 2
         for n in nums:
             cnt[n] = cnt.get(n, 0) + 1
-        max_cnt = max(cnt.values())
-        for k, v in cnt.items():
-            if v == max_cnt:
-                return k
+            if cnt[n] > majority:
+                return n
+
+        # time: O(n), space: O(n)
+        # cnt = {}
+        # for n in nums:
+        #     cnt[n] = cnt.get(n, 0) + 1
+        # max_cnt = max(cnt.values())
+        # for k, v in cnt.items():
+        #     if v == max_cnt:
+        #         return k
