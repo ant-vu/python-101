378558c40910842ea44172b16a433967882d58a6
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-05 18:01:55 -0400

    feat: add solution

--- 171.py
+++ 171.py
@@ -1,10 +1,16 @@
 class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
-        # time: O(n), space: O(n)
-        tmp = []
+        # time: O(n), space: O(1)
         res = 0
         for c in columnTitle:
-            tmp = [26 - (ord("Z") - ord(c))] + tmp
-        for i in range(len(tmp)):
-            res += tmp[i] * (26 ** i)
+            res = (res * 26) + (ord(c) - ord("A") + 1)
         return res
+
+        # time: O(n), space: O(n)
+        # tmp = []
+        # res = 0
+        # for c in columnTitle:
+        #     tmp = [26 - (ord("Z") - ord(c))] + tmp
+        # for i in range(len(tmp)):
+        #     res += tmp[i] * (26 ** i)
+        # return res
