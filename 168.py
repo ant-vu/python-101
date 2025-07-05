8c1c7dc76f9bc6b91683b31d1e927e328f3f83e7
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-05 18:20:01 -0400

    feat: add solution

---
+++ 168.py
@@ -0,0 +1,9 @@
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        # time: O(logn), space: O(1)
+        res = ""
+        while columnNumber > 0:
+            columnNumber -= 1
+            res += chr(columnNumber % 26 + ord("A"))
+            columnNumber //= 26
+        return res[::-1]
