6623cd080f5851aaef681011e038ab85052e04d4
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-04 21:07:25 -0400

    feat: add solution

--- 144.py
+++ 144.py
@@ -7,11 +7,25 @@
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         # time: O(n), space: O(n)
+        if not root:
+            return []
         res = []
-        def dfs(node):
-            if node:
-                res.append(node.val)
-                dfs(node.left)
-                dfs(node.right)
-        dfs(root)
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
         return res
+
+        # time: O(n), space: O(n)
+        # res = []
+        # def dfs(node):
+        #     if node:
+        #         res.append(node.val)
+        #         dfs(node.left)
+        #         dfs(node.right)
+        # dfs(root)
+        # return res
