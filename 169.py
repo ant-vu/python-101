31569eb1e26d2c335256ba9ec1b588c067a354f8
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-05 17:31:10 -0400

    feat: add solution

--- 169.py
+++ 169.py
@@ -1,12 +1,20 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # time: O(n), space: O(n)
-        cnt = {}
-        majority = len(nums) // 2
+        # time: O(n), space: O(1)
+        res, majority = 0, 0
         for n in nums:
-            cnt[n] = cnt.get(n, 0) + 1
-            if cnt[n] > majority:
-                return n
+            if majority == 0:
+                res = n
+            majority += 1 if n == res else -1
+        return res
+
+        # time: O(n), space: O(n)
+        # cnt = {}
+        # majority = len(nums) // 2
+        # for n in nums:
+        #     cnt[n] = cnt.get(n, 0) + 1
+        #     if cnt[n] > majority:
+        #         return n
 
         # time: O(n), space: O(n)
         # cnt = {}
