8f29964c2246dcfa8a557f0aa8ca5babee933d32
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-06 14:24:56 -0400

    feat: add solution

--- 202.py
+++ 202.py
@@ -1,11 +1,29 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        # time: O(n), space: O(n)
-        seen = set()
-        while True:
-            if n == 1:
+        # time: O(logn), space: O(1)
+        def getNextNumber(n):
+            output = 0
+            while n:
+                digit = n % 10
+                output += digit ** 2
+                n //= 10
+            return output
+        
+        slow = getNextNumber(n)
+        fast = getNextNumber(getNextNumber(n))
+        while slow != fast:
+            if fast == 1:
                 return True
-            n = sum([int(x) ** 2 for x in str(n)])
-            if n in seen:
-                return False
-            seen.add(n)
+            slow = getNextNumber(slow)
+            fast = getNextNumber(getNextNumber(fast))
+        return slow == 1
+
+        # time: O(n), space: O(n)
+        # seen = set()
+        # while True:
+        #     if n == 1:
+        #         return True
+        #     n = sum([int(x) ** 2 for x in str(n)])
+        #     if n in seen:
+        #         return False
+        #     seen.add(n)
