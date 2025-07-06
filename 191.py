36cd85ee82be5c5874b575512a6012e2369da352
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-06 14:08:44 -0400

    feat: add solution

--- 191.py
+++ 191.py
@@ -2,7 +2,13 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         # time: O(1), space: O(1)
         cnt = 0
-        while n:
-            cnt += 1 if n & 1 else 0
-            n >>= 1
+        for i in range(32):
+            cnt += 1 if (n >> i) & 1 else 0
         return cnt
+
+        # time: O(1), space: O(1)
+        # cnt = 0
+        # while n:
+        #     cnt += 1 if n & 1 else 0
+        #     n >>= 1
+        # return cnt
