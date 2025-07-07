18d60469a339f60281b54ab4ef131f3947d0b594
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-07 10:49:33 -0400

    feat: add solution

--- 222.py
+++ 222.py
@@ -6,16 +6,32 @@
 #         self.right = right
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
+        # time: O(logn * logn), space: O(logn)
+        def getDepth(node, left):
+            depth = 0
+            while node:
+                node = node.left if left else node.right
+                depth += 1
+            return depth
+        
+        if not root:
+            return 0
+        right = getDepth(root.left, True) + 1
+        left = getDepth(root.right, False) + 1
+        if right == left:
+            return (2 ** right) - 1
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+
         # time: O(n), space: O(n)
-        q = deque()
-        q.append(root)
-        cnt = 0
-        while q:
-            node = q.popleft()
-            if node:
-                cnt += 1
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-        return cnt
+        # q = deque()
+        # q.append(root)
+        # cnt = 0
+        # while q:
+        #     node = q.popleft()
+        #     if node:
+        #         cnt += 1
+        #         if node.left:
+        #             q.append(node.left)
+        #         if node.right:
+        #             q.append(node.right)
+        # return cnt
