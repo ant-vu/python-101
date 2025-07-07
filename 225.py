68c43992df74d916b6b90b9ab474bd9d25e28b23
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-07 11:16:11 -0400

    feat: add solution

---
+++ 225.py
@@ -0,0 +1,28 @@
+class MyStack:
+
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        # time: O(n), space: O(n)
+        self.q.append(x)
+        for _ in range(len(self.q) - 1):
+            tmp = self.q.popleft()
+            self.q.append(tmp)
+
+    def pop(self) -> int:
+        return self.q.popleft()
+
+    def top(self) -> int:
+        return self.q[0]
+
+    def empty(self) -> bool:
+        return not self.q
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
