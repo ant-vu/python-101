b9433a922425c4612aba5ec18c726c16088eef40
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-07 09:51:37 -0400

    feat: add solution

--- 217.py
+++ 217.py
@@ -1,9 +1,16 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # time: O(n), space: O(n)
-        seen = set()
-        for n in nums:
-            if n in seen:
+        # time: O(nlogn), space: O(1)
+        nums.sort()
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
                 return True
-            seen.add(n)
         return False
+
+        # time: O(n), space: O(n)
+        # seen = set()
+        # for n in nums:
+        #     if n in seen:
+        #         return True
+        #     seen.add(n)
+        # return False
