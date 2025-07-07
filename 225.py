368230484b81d1ce91cc00d257d34693d7392a9d
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-07 11:17:18 -0400

    feat: add solution

--- 225.py
+++ 225.py
@@ -7,8 +7,7 @@ class MyStack:
         # time: O(n), space: O(n)
         self.q.append(x)
         for _ in range(len(self.q) - 1):
-            tmp = self.q.popleft()
-            self.q.append(tmp)
+            self.q.append(self.q.popleft())
 
     def pop(self) -> int:
         return self.q.popleft()
