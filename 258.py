961ae42bf34aa49d06e67949a173d78aa1361e87
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-09 09:10:56 -0400

    feat: add solution

--- 258.py
+++ 258.py
@@ -1,6 +1,11 @@
 class Solution:
     def addDigits(self, num: int) -> int:
+        # time: O(1), space: O(1)
+        if num == 0:
+            return 0
+        return num % 9 if num % 9 != 0 else 9
+
         # time: O(n), space: O(n)
-        if len(str(num)) == 1:
-            return num
-        return self.addDigits(sum([int(x) for x in str(num)]))
+        # if len(str(num)) == 1:
+        #     return num
+        # return self.addDigits(sum([int(x) for x in str(num)]))
