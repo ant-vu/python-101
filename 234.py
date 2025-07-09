f759b5c5c52e7e3af6a333d1d68975052089fc9d
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-08 21:19:41 -0400

    feat: add solution

--- 234.py
+++ 234.py
@@ -5,9 +5,30 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # time: O(n), space: O(1)
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        node = None
+        while slow:
+            tmp = slow.next
+            slow.next = node
+            node = slow
+            slow = tmp
+        
+        first, second = head, node
+        while second:
+            if first.val != second.val:
+                return False
+            first = first.next
+            second = second.next
+        return True
+
         # time: O(n), space: O(n)
-        vals = []
-        while head:
-            vals.append(head.val)
-            head = head.next
-        return vals == vals[::-1]
+        # vals = []
+        # while head:
+        #     vals.append(head.val)
+        #     head = head.next
+        # return vals == vals[::-1]
