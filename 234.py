1026839ef49ef362615a1152221ad84914d1d87d
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-08 21:15:59 -0400

    feat: add solution

---
+++ 234.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # time: O(n), space: O(n)
+        vals = []
+        while head:
+            vals.append(head.val)
+            head = head.next
+        return vals == vals[::-1]
