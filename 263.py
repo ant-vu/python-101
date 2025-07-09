0575241c996547ecf82bae4ba1eb9481e60a787c
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-09 09:17:00 -0400

    feat: add solution

--- 263.py
+++ 263.py
@@ -1,12 +1,20 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
         # time: O(n), space: O(1)
-        if n == 0:
+        if n <= 0:
             return False
-        while n % 5 == 0:
-            n //= 5
-        while n % 3 == 0:
-            n //= 3
-        while n % 2 == 0:
-            n //= 2
+        for p in (2, 3, 5):
+            while n % p == 0:
+                n //= p
         return n == 1
+
+        # time: O(n), space: O(1)
+        # if n <= 0:
+        #     return False
+        # while n % 5 == 0:
+        #     n //= 5
+        # while n % 3 == 0:
+        #     n //= 3
+        # while n % 2 == 0:
+        #     n //= 2
+        # return n == 1
