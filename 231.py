2ae287019ee25b458dbb787728337817b4930166
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-08 21:02:42 -0400

    feat: add solution

--- 231.py
+++ 231.py
@@ -1,5 +1,8 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
+        # time: O(1), space: O(1)
+        return n > 0 and (n & (n - 1)) == 0
+
         # time: O(n), space: O(n)
-        powers = [2 ** x for x in range(32)]
-        return n in powers
+        # powers = [2 ** x for x in range(32)]
+        # return n in powers
