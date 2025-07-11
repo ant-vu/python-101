d0e9a3c2dad14ce713cc62a4e04c3e9b0a34e29e
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-10 22:17:12 -0400

    feat: add solution

--- 342.py
+++ 342.py
@@ -1,4 +1,7 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
+        # time: O(1), space: O(1)
+        return log(n, 4).is_integer() if n > 0 else False
+
         # time: O(n), space: O(n)
-        return n in [4 ** x for x in range(16)]
+        # return n in [4 ** x for x in range(16)]
