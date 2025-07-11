e48a9a4db3ec7b83fb1f175009db03b733813d6f
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-10 22:07:38 -0400

    feat: add solution

--- 326.py
+++ 326.py
@@ -1,4 +1,7 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
+        # time: O(1), space: O(1)
+        return (3 ** 19) % n == 0 if n > 0 else False
+
         # time: O(n), space: O(n)
-        return n in [3 ** x for x in range(32)]
+        # return n in [3 ** x for x in range(19)]
