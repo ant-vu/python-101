e9c6b6802c36c7dc48c2d5a7ac4a273ca822922f
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-23 13:48:28 -0400

    feat: add solution

--- 62.py
+++ 62.py
@@ -1,8 +1,17 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # time: O(m * n), space: O(m * n)
-        dp = [[1] * n] * m
-        for r in range(1, m):
+        # time: O(m * n), space: O(n)
+        aboveRow = [1] * n
+        for _ in range(m - 1):
+            curRow = [1] * n
             for c in range(1, n):
-                dp[r][c] = dp[r - 1][c] + dp[r][c - 1]
-        return dp[m - 1][n - 1]
+                curRow[c] = curRow[c - 1] + aboveRow[c]
+            aboveRow = curRow
+        return aboveRow[-1]
+
+        # time: O(m * n), space: O(m * n)
+        # grid = [[1 for _ in range(n)] for _ in range(m)]
+        # for r in range(1, m):
+        #     for c in range(1, n):
+        #         grid[r][c] = grid[r - 1][c] + grid[r][c - 1]
+        # return grid[-1][-1]
