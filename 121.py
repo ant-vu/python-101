7eb81cbdac6a53cab56d13f051a7c943e25ba606
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-23 21:52:34 -0400

    feat: add solution

--- 121.py
+++ 121.py
@@ -1,9 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         # time: O(n), space: O(1)
-        res = 0
-        mini = prices[0]
-        for i in prices:
-            res = max(i - mini, res)
-            mini = min(i, mini)
-        return res
+        maxProfit = 0
+        minSeen = prices[0]
+        for i in range(1, len(prices)):
+            maxProfit = max(maxProfit, prices[i] - minSeen)
+            minSeen = min(minSeen, prices[i])
+        return maxProfit
