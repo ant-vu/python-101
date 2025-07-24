d7e08679a45f61420dc4cd84f4e79adf948dcada
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-23 20:38:27 -0400

    feat: add solution

--- 72.py
+++ 72.py
@@ -1,11 +1,12 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         # time: O(m * n), space: O(m * n)
-        m, n = len(word1), len(word2)
+        m = len(word1)
+        n = len(word2)
         dp = [[0] * (n + 1) for _ in range(m + 1)]
-        for i in range(m + 1):
+        for i in range(1, m + 1):
             dp[i][0] = i
-        for j in range(n + 1):
+        for j in range(1, n + 1):
             dp[0][j] = j
         for i in range(1, m + 1):
             for j in range(1, n + 1):
