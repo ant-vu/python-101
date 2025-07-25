a97d904d5961aea18dc936530385885019eb5e39
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-24 20:58:25 -0400

    feat: add solution

--- 88.py
+++ 88.py
@@ -4,14 +4,25 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         # time: O(m + n), space: O(1)
-        idx = m + n - 1
-        m -= 1
-        n -= 1
-        while n >= 0:
-            if m >= 0 and nums1[m] > nums2[n]:
-                nums1[idx] = nums1[m]
-                m -= 1
+        idx1 = m - 1
+        idx2 = n - 1
+        curIdx = m + n - 1
+        while idx2 >= 0:
+            if idx1 >= 0 and nums1[idx1] > nums2[idx2]:
+                nums1[curIdx] = nums1[idx1]
+                idx1 -= 1
             else:
-                nums1[idx] = nums2[n]
-                n -= 1
-            idx -= 1
+                nums1[curIdx] = nums2[idx2]
+                idx2 -= 1
+            curIdx -= 1
+
+        # time: O(m + n), space: O(1)
+        # idx1 = m - 1
+        # idx2 = n - 1
+        # for curIdx in range(m + n - 1, -1, -1):
+        #     if (idx1 >= 0 and idx2 >= 0 and nums1[idx1] > nums2[idx2]) or idx2 < 0:
+        #         nums1[curIdx] = nums1[idx1]
+        #         idx1 -= 1
+        #     else:
+        #         nums1[curIdx] = nums2[idx2]
+        #         idx2 -= 1
