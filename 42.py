1287d9e7cbe9218ed505ac6e7289526a39395700
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-25 18:50:05 -0400

    feat: add solution

--- 42.py
+++ 42.py
@@ -1,18 +1,36 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         # time: O(n), space: O(1)
-        left = 0
-        right = len(height) - 1
-        left_max = height[left]
-        right_max = height[right]
+        n = len(height)
         water = 0
-        while left < right:
-            if left_max < right_max:
-                left += 1
-                left_max = max(left_max, height[left])
-                water += left_max - height[left]
+        l = 0
+        r = n - 1
+        maxL = height[l]
+        maxR = height[r]
+        while l < r:
+            if maxL < maxR:
+                l += 1
+                maxL = max(maxL, height[l])
+                water += maxL - height[l]
             else:
-                right -= 1
-                right_max = max(right_max, height[right])
-                water += right_max - height[right]
+                r -= 1
+                maxR = max(maxR, height[r])
+                water += maxR - height[r]
         return water
+
+        # time: O(n^2), space: O(1), TLE
+        # n = len(height)
+        # water = 0
+        # l = 0
+        # r = n - 1
+        # while l + 1 < r:
+        #     for i in range(l + 1, r):
+        #         wall = min(height[l], height[r])
+        #         if wall - height[i] > 0:
+        #             water += wall - height[i]
+        #             height[i] = wall
+        #     if height[l] < height[r]:
+        #         l += 1
+        #     else:
+        #         r -= 1
+        # return water
