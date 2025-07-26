2d6d7a038a4ec03155e232094f641187f0eca536
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-25 22:04:11 -0400

    feat: add solution

--- 1.py
+++ 1.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # time: O(n), space: O(n)
-        mapping = {}
-        for idx, num in enumerate(nums):
-            if target - num in mapping:
-                return [mapping[target - num], idx]
-            mapping[num] = idx
+        seenToIdx = {}
+        for i, n in enumerate(nums):
+            if target - n in seenToIdx:
+                return [i, seenToIdx[target - n]]
+            seenToIdx[n] = i
