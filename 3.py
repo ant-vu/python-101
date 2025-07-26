8cd6ae6c845cd5c29770c3fe8f96fe4ce110b897
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-26 10:44:12 -0400

    feat: add solution

--- 3.py
+++ 3.py
@@ -1,36 +1,36 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         # time: O(n), space: O(1)
-        last_seen = {}
-        res = 0
+        lastSeen = {}
+        maxCount = 0
         l = 0
         for r, c in enumerate(s):
-            if c in last_seen and last_seen[c] >= l:
-                l = last_seen[c] + 1
-            res = max(r - l + 1, res)
-            last_seen[c] = r
-        return res
+            if c in lastSeen and lastSeen[c] >= l:
+                l = lastSeen[c] + 1
+            lastSeen[c] = r
+            maxCount = max(maxCount, r - l + 1)
+        return maxCount
 
         # time: O(n), space: O(1)
         # count = {}
-        # res = 0
+        # maxCount = 0
         # l = 0
         # for r, c in enumerate(s):
         #     count[c] = count.get(c, 0) + 1
         #     while count[c] > 1:
         #         count[s[l]] -= 1
         #         l += 1
-        #     res = max(r - l + 1, res)
-        # return res
+        #     maxCount = max(maxCount, r - l + 1)
+        # return maxCount
 
         # time: O(n), space: O(1)
-        # unique = set()
-        # res = 0
+        # seen = set()
+        # maxCount = 0
         # l = 0
         # for r in range(len(s)):
-        #     while s[r] in unique:
-        #         unique.remove(s[l])
+        #     while s[r] in seen:
+        #         seen.remove(s[l])
         #         l += 1
-        #     unique.add(s[r])
-        #     res = max(r - l + 1, res)
-        # return res
+        #     seen.add(s[r])
+        #     maxCount = max(maxCount, r - l + 1)
+        # return maxCount
