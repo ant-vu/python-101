0c83f41a94c40fff9e215c0d8f58ace9a3880858
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-27 14:33:26 -0400

    feat: add solution

--- 383.py
+++ 383.py
@@ -1,12 +1,13 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        # time: O(m + n), space: O(m + n)
-        cntr, cntm = {}, {}
-        for c in ransomNote:
-            cntr[c] = cntr.get(c, 0) + 1
+        # time: O(m + n), space: O(1)
+        count = {}
         for c in magazine:
-            cntm[c] = cntm.get(c, 0) + 1
-        for k, v in cntr.items():
-            if k not in cntm or cntm[k] < v:
+            count[c] = count.get(c, 0) + 1
+        for c in ransomNote:
+            if c not in count:
+                return False
+            count[c] -= 1
+            if count[c] < 0:
                 return False
         return True
