89f589459773713ee4cb8e7bf236c9babab81cea
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-28 19:49:21 -0400

    feat: add solution

--- 219.py
+++ 219.py
@@ -1,32 +1,9 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         # time: O(n), space: O(n)
-        seen = set()
-        for i, val in enumerate(nums):
-            if i > k:
-                seen.remove(nums[i - k - 1])
-            if val in seen:
+        lastIdx = {}
+        for i, n in enumerate(nums):
+            if n in lastIdx and i - lastIdx[n] <= k:
                 return True
-            seen.add(val)
+            lastIdx[n] = i
         return False
-
-        # time: O(n), space: O(n)
-        # seen = {}
-        # for i, val in enumerate(nums):
-        #     if val in seen and i - seen[val] <= k:
-        #         return True
-        #     seen[val] = i
-        # return False
-
-        # time: O(n), space: O(n)
-        # cur = set()
-        # for n in nums[: k + 1]:
-        #     if n in cur:
-        #         return True
-        #     cur.add(n)
-        # for i in range(k + 1, len(nums)):
-        #     cur.remove(nums[i - k - 1])
-        #     if nums[i] in cur:
-        #         return True
-        #     cur.add(nums[i])
-        # return False
