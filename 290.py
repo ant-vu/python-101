e94654c6319abf62cdba8bb6ae69420dc9bf5672
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-28 19:00:24 -0400

    feat: add solution

--- 290.py
+++ 290.py
@@ -1,12 +1,14 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        # time: O(n), space: O(n)
-        s = s.split(" ")
-        if len(pattern) != len(s):
+        # time: O(m + n), space: O(m + n)
+        if len(pattern) != len(s.split()):
             return False
         mapping = {}
-        for a, b in zip(pattern, s):
-            if (a in mapping and mapping[a] != b) or (a not in mapping and b in mapping.values()):
+        for a, b in zip(pattern, s.split()):
+            if a in mapping:
+                if mapping[a] != b:
+                    return False
+            elif b in mapping.values():
                 return False
             mapping[a] = b
         return True
