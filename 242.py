e6f74ce1e2dd7f85782c2a46eae0ef43a9566832
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-28 19:09:49 -0400

    feat: add solution

--- 242.py
+++ 242.py
@@ -3,8 +3,11 @@ class Solution:
         # time: O(m + n), space: O(m + n)
         if len(s) != len(t):
             return False
-        cnts1, cnts2 = {}, {}
-        for i, j in zip(s, t):
-            cnts1[i] = cnts1.get(i, 0) + 1
-            cnts2[j] = cnts2.get(j, 0) + 1
-        return cnts1 == cnts2
+        mapS = {}
+        for c in s:
+            mapS[c] = mapS.get(c, 0) + 1
+        for c in t:
+            if c not in mapS or mapS[c] == 0:
+                return False
+            mapS[c] -= 1
+        return True
