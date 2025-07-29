49b8c1798a2c43e822f13497a59b1433fa9b9462
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-29 18:55:05 -0400

    feat: add solution

--- 228.py
+++ 228.py
@@ -4,40 +4,15 @@ class Solution:
         res = []
         if not nums:
             return res
-        start = nums[0]
-        end = nums[0]
-        for n in nums[1:]:
-            if n == end + 1:
-                end = n
+        i = 0
+        while i < len(nums):
+            start = nums[i]
+            while i < len(nums) - 1 and nums[i] == nums[i + 1] - 1:
+                i += 1
+            end = nums[i]
+            if start == end:
+                res.append(str(start))
             else:
-                if start == end:
-                    res.append(str(start))
-                else:
-                    res.append(str(start) + "->" + str(end))
-                start = n
-                end = n
-        if start == end:
-            res.append(str(start))
-        else:
-            res.append(str(start) + "->" + str(end))
+                res.append(str(start) + "->" + str(end))
+            i += 1
         return res
-
-        # time: O(n), space: O(n)
-        # if len(nums) == 1:
-        #     return [str(nums[0])]
-        # ranges = []
-        # i = 0
-        # while i < len(nums) - 1:
-        #     cur = str(nums[i])
-        #     flg = False
-        #     while i < len(nums) - 1 and nums[i] == nums[i + 1] - 1:
-        #         i += 1
-        #         flg = True
-        #     if flg:
-        #         ranges.append(cur + "->" + str(nums[i]))
-        #     else:
-        #         ranges.append(cur)
-        #     i += 1
-        # if len(nums) >= 2 and nums[-2] != nums[-1] - 1:
-        #     ranges.append(str(nums[-1]))
-        # return ranges
