e3009ea7aee40e6b62fb46be32ce490e640b9eca
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-07-29 20:27:06 -0400

    feat: add solution

--- 20.py
+++ 20.py
@@ -1,26 +1,12 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         # time: O(n), space: O(n)
-        def is_pair(last, cur):
-            if last == "(" and cur == ")" or last == "{" and cur == "}" or last == "[" and cur == "]":
-                return True
-            return False
-
         stack = []
-        for i in range(len(s)):
-            if stack:
-                if is_pair(stack[-1], s[i]):
-                    stack.pop()
-                    continue
-            stack.append(s[i])
+        mapping = {")": "(", "}": "{", "]": "["}
+        for c in s:
+            if c in mapping:
+                if not stack or stack.pop() != mapping[c]:
+                    return False
+            else:
+                stack.append(c)
         return not stack
-
-        # time: O(n), space: O(n)
-        # stack = []
-        # mapping = {')': '(', '}': '{', ']': '['}
-        # for char in s:
-        #     if char not in mapping:
-        #         stack.append(char)
-        #     elif not stack or stack.pop() != mapping[char]:
-        #         return False
-        # return not stack
