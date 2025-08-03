462edde057337d38dc63557e902d898145f1a774
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-08-03 14:58:26 -0400

    feat: add solution

---
+++ 82.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # time: O(n), space: O(1)
+        dummy = ListNode(next=head)
+        prev = dummy
+        while head and head.next:
+            if head.val == head.next.val:
+                while head.next and head.val == head.next.val:
+                    head = head.next
+                prev.next = head.next
+            else:
+                prev = prev.next
+            head = head.next
+        return dummy.next
