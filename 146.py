165a8656c422a0e1f692e8ed7dbbd439c0cc376c
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-08-03 18:58:16 -0400

    feat: add solution

--- 146.py
+++ 146.py
@@ -1,14 +1,15 @@
 class Node:
-    def __init__(self, key, val):
-        self.key = key
-        self.val = val
+
+    def __init__(self, nodeKey, nodeValue):
+        self.nodeKey = nodeKey
+        self.nodeValue = nodeValue
         self.prev = None
         self.next = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cap = capacity
+        self.capacity = capacity
         self.cache = {}
         self.oldest = Node(0, 0)
         self.latest = Node(0, 0)
@@ -20,8 +21,22 @@ class LRUCache:
         if key in self.cache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
-            return self.cache[key].val
+            return self.cache[key].nodeValue
         return -1
+    
+    def remove(self, node):
+        prev = node.prev
+        cur = node.next
+        prev.next = cur
+        cur.prev = prev
+    
+    def insert(self, node):
+        prev = self.latest.prev
+        cur = self.latest
+        prev.next = node
+        cur.prev = node
+        node.next = cur
+        node.prev = prev
 
     def put(self, key: int, value: int) -> None:
         # time: O(1), space: O(n)
@@ -29,21 +44,10 @@ class LRUCache:
             self.remove(self.cache[key])
         self.cache[key] = Node(key, value)
         self.insert(self.cache[key])
-        if len(self.cache) > self.cap:
+        if len(self.cache) > self.capacity:
             lru = self.oldest.next
             self.remove(lru)
-            del self.cache[lru.key]
-    
-    def remove(self, node):
-        prev, next = node.prev, node.next
-        prev.next = next
-        next.prev = prev
-
-    def insert(self, node):
-        prev, next = self.latest.prev, self.latest
-        prev.next = next.prev = node
-        node.next = next
-        node.prev = prev
+            del self.cache[lru.nodeKey]
 
 
 # Your LRUCache object will be instantiated and called as such:
