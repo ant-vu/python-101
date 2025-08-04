c6eeb889a57b7f804d1d2f848c168c04d2d33f42
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-08-03 20:56:27 -0400

    feat: add solution

--- 100.py
+++ 100.py
@@ -6,7 +6,9 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        # time: O(n), space: O(n)
-        if not p or not q:
-            return p == q
-        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        # time: O(n), space: O(h)
+        if not p and not q:
+            return True
+        if (p and not q) or (not p and q) or (p.val != q.val):
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
