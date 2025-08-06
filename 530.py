7e20a569224e60905ff7dbc51c5655ba86a09a55
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-08-05 22:20:25 -0400

    feat: add solution

--- 530.py
+++ 530.py
@@ -6,19 +6,16 @@
 #         self.right = right
 class Solution:
     def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
-        # time: O
+        # time: O(n), space: O(n)
+        def inorder(node):
+            if node:
+                inorder(node.left)
+                nodes.append(node.val)
+                inorder(node.right)
+
         nodes = []
-        q = deque()
-        q.append(root)
-        while q:
-            node = q.popleft()
-            nodes.append(node.val)
-            if node.left:
-                q.append(node.left)
-            if node.right:
-                q.append(node.right)
-        res = inf
-        nodes.sort()
+        inorder(root)
+        res = float("inf")
         for i in range(len(nodes) - 1):
-            res = min(res, abs(nodes[i] - nodes[i + 1]))
+            res = min(res, nodes[i + 1] - nodes[i])
         return res
