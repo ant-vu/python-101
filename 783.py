2ff493a94255938f2d9caad10cc8bfa07ba53e6c
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-08-05 22:21:49 -0400

    feat: add solution

---
+++ 783.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        # time: O(n), space: O(n)
+        def inorder(node):
+            if node:
+                inorder(node.left)
+                nodes.append(node.val)
+                inorder(node.right)
+
+        nodes = []
+        inorder(root)
+        res = float("inf")
+        for i in range(len(nodes) - 1):
+            res = min(res, nodes[i + 1] - nodes[i])
+        return res
