4e9fa46d5923d3c535640c1813e0fe60c7abdda1
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-08-06 20:25:05 -0400

    feat: add solution

--- 21.py
+++ 21.py
@@ -5,19 +5,27 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # time: O(m + n), space: O(m + n)
+        if not list1 or not list2:
+            return list1 if list1 else list2
+        if list1.val < list2.val:
+            list1, list2 = list2, list1
+        list2.next = self.mergeTwoLists(list1, list2.next)
+        return list2
+
         # time: O(m + n), space: O(1)
-        dummy = ListNode()
-        res = dummy
-        while list1 and list2:
-            if list1.val < list2.val:
-                dummy.next = list1
-                list1 = list1.next
-            else:
-                dummy.next = list2
-                list2 = list2.next
-            dummy = dummy.next
-        if list1:
-            dummy.next = list1
-        else:
-            dummy.next = list2
-        return res.next
+        # dummy = ListNode()
+        # cur = dummy
+        # while list1 and list2:
+        #     if list1.val < list2.val:
+        #         cur.next = list1
+        #         list1 = list1.next
+        #     else:
+        #         cur.next = list2
+        #         list2 = list2.next
+        #     cur = cur.next
+        # if list1:
+        #     cur.next = list1
+        # elif list2:
+        #     cur.next = list2
+        # return dummy.next
