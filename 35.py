5725c08c38c0d1f61f4120225e46e1d5333d6533
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-08-06 20:47:36 -0400

    feat: add solution

--- 35.py
+++ 35.py
@@ -1,14 +1,14 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        # time: O(logn), space: O(1)
-        left = 0
-        right = len(nums) - 1
-        while left <= right:
-            mid = left + (right - left) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
-                left = mid + 1
+        # time: O(log(n)), space: O(1)
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            m = l + (r - l) // 2
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m + 1
             else:
-                right = mid - 1
-        return left
+                r = m - 1
+        return l
