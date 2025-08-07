47b71ebd97a52c9a9b6f778b73c680086275eee0
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-08-06 21:30:31 -0400

    feat: add solution

--- 34.py
+++ 34.py
@@ -1,38 +1,23 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        # time: O(logn), space: O(1)
-        def binary_search(nums, target, is_searching_right):
-            l, r = 0, len(nums) - 1
-            idx = -1
-            while l <= r:
-                m = l + (r - l) // 2
-                if nums[m] < target:
-                    l = m + 1
-                elif nums[m] > target:
-                    r = m - 1
-                else:
-                    idx = m
-                    if is_searching_right:
-                        l = m + 1
-                    else:
-                        r = m - 1
-            return idx
-        return [binary_search(nums, target, False), binary_search(nums, target, True)]
-
-
-        # time: O(logn), space: O(1)
-        # l, r = 0, len(nums) - 1
-        # while l <= r:
-        #     m = l + (r - l) // 2
-        #     if nums[m] == target:
-        #         l2 = r2 = m
-        #         while l2 >= 0 and nums[l2] == target:
-        #             l2 -= 1
-        #         while r2 <= len(nums) - 1 and nums[r2] == target:
-        #             r2 += 1
-        #         return [l2 + 1, r2 - 1]
-        #     elif nums[m] < target:
-        #         l = m + 1
-        #     else:
-        #         r = m - 1
-        # return [-1, -1]
+        # time: O(log(n)), space: O(1)
+        foundTarget = False
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            m = l + (r - l) // 2
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        l2 = 0
+        r2 = len(nums) - 1
+        while l2 <= r2:
+            m = l2 + (r2 - l2) // 2
+            if nums[m] <= target:
+                if nums[m] == target:
+                    foundTarget = True
+                l2 = m + 1
+            else:
+                r2 = m - 1
+        return [l, r2] if foundTarget else [-1, -1]
