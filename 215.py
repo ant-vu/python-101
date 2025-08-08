96859d83db695a3eac1489c1bff4e90670c42af3
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-08-07 22:25:15 -0400

    feat: add solution

--- 215.py
+++ 215.py
@@ -1,16 +1,19 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        # time: O(nlogk), space: O(k)
-        min_heap = []
+        # time: O(nlog(k)), space: O(k)
+        heap = []
         for n in nums:
-            heapq.heappush(min_heap, n)
-            if len(min_heap) > k:
-                heapq.heappop(min_heap)
-        return min_heap[0]
+            if len(heap) < k:
+                heapq.heappush(heap, n)
+            else:
+                heapq.heappushpop(heap, n)
+        return heap[0]
 
-        # time: O(nlogn), space: O(n)
-        # nums = [-n for n in nums]
+        # time: O(nlog(n)), space: O(1)
         # heapq.heapify(nums)
-        # for i in range(k - 1):
+        # for _ in range(len(nums) - k):
         #     heapq.heappop(nums)
-        # return -heapq.heappop(nums)
+        # return heapq.heappop(nums)
+
+        # time: O(nlog(k)), space: O(k)
+        # return heapq.nlargest(k, nums)[-1]
