1c9f534a9c90778e8852785f37e1186d8ad1a01b
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-08-08 20:28:44 -0400

    feat: add solution

--- 69.py
+++ 69.py
@@ -1,16 +1,16 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        # time: O(logn), space: O(1)
+        # time: O(n), space: O(1)
         if x == 1:
             return 1
-        low = 0
-        high = x // 2
-        while low <= high:
-            mid = low + (high - low) // 2
-            if mid ** 2 == x:
-                return mid
-            elif mid ** 2 < x:
-                low = mid + 1
+        l = 0
+        r = x // 2
+        while l <= r:
+            m = l + (r - l) // 2
+            if m * m == x:
+                return m
+            elif m * m < x:
+                l = m + 1
             else:
-                high = mid - 1
-        return high
+                r = m - 1
+        return r
