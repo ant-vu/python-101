f6fa4d2403ea9730e6a0e2bff3a38fb9e709fd14
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-08-08 20:13:07 -0400

    feat: add solution

--- 191.py
+++ 191.py
@@ -1,14 +1,4 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        # time: O(1), space: O(1)
-        cnt = 0
-        for i in range(32):
-            cnt += 1 if (n >> i) & 1 else 0
-        return cnt
-
-        # time: O(1), space: O(1)
-        # cnt = 0
-        # while n:
-        #     cnt += 1 if n & 1 else 0
-        #     n >>= 1
-        # return cnt
+        # time: O(n), space: O(1)
+        return str(bin(n)[2:]).count("1")
