eaefbc7b40b3b7befc4fb58dceedd6b9f0484196
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-08-08 20:14:26 -0400

    feat: add solution

--- 136.py
+++ 136.py
@@ -1,7 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         # time: O(n), space: O(1)
-        bit = nums[0]
-        for i in range(1, len(nums)):
-            bit ^= nums[i]
-        return bit
+        res = nums[0]
+        for n in nums[1:]:
+            res ^= n
+        return res
