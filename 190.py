1fbfe67eba36d982dc83a94abe47ed349323888d
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-08-08 20:10:45 -0400

    feat: add solution

--- 190.py
+++ 190.py
@@ -1,13 +1,4 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        # time: O(1), space: O(1)
-        res = 0
-        for i in range(32):
-            res = (res << 1) | (n & 1)
-            n >>= 1
-        return res
-
-        # time: O(1), space: O(1)
-        # n = str(bin(n)[2:])[::-1]
-        # n += "0" * (32 - len(n))
-        # return int(n, 2)
+        # time: O(n), space: O(1)
+        return int(bin(n)[2:].zfill(32)[::-1], 2)
