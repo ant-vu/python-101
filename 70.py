582c18d8736d65ca3babcf0e9688b4ca4dddbaa1
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-08-18 21:31:24 -0400

    feat: solution

--- 70.py
+++ 70.py
@@ -1,12 +1,27 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        # time: O(n), space: O(n)
+        def helper(x):
+            if x in memo:
+                return memo[x]
+            memo[x] = helper(x - 1) + helper(x - 2)
+            return memo[x]
+        memo = {1: 1, 2: 2}
+        return helper(n)
+
         # time: O(n), space: O(1)
-        if n <= 3:
-            return n
-        left = 2
-        mid = 3
-        for _ in range(n - 3):
-            right = left + mid
-            left = mid
-            mid = right
-        return right
+        # if n <= 2:
+        #     return n
+        # a = 1
+        # b = 2
+        # for _ in range(3, n + 1):
+        #     a, b = b, a + b
+        # return b
+
+        # time: O(n), space: O(n)
+        # dp = [0] * (n + 2)
+        # dp[1] = 1
+        # dp[2] = 2
+        # for i in range(3, n + 1):
+        #     dp[i] = dp[i - 1] + dp[i - 2]
+        # return dp[n]
