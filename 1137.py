9a306be05b82eee463e7f992e91dd68a3bec4a07
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-08-18 21:47:02 -0400

    feat: solution

--- 1137.py
+++ 1137.py
@@ -1,9 +1,17 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
         # time: O(n), space: O(1)
-        if n == 0:
-            return 0
-        x, y, z = 0, 1, 1
-        for _ in range(n - 2):
-            x, y, z = y, z, x + y + z
-        return z
+        a, b, c = 1, 0, 0
+        for _ in range(n):
+            a, b, c = b, c, a + b + c
+        return c
+
+        # time: O(n), space: O(1)
+        # if n <= 1:
+        #     return n
+        # elif n == 2:
+        #     return 1
+        # a, b, c = 0, 1, 1
+        # for _ in range(n - 2):
+        #     a, b, c = b, c, a + b + c
+        # return c
