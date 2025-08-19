fe15c01fe382abedcaa262adbf8ffa36777e881a
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-08-18 23:10:58 -0400

    feat: solution

--- 740.py
+++ 740.py
@@ -1,14 +1,10 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
         # time: O(n), space: O(n)
-        houses = [0] * (max(nums) + 1)
-        for i in nums:
-            houses[i] += i
-        if len(houses) < 2:
-            return houses[0]
-        l = houses[0]
-        r = max(houses[0], houses[1])
-        for i in range(2, len(houses)):
-            cur = max(l + houses[i], r)
-            l, r = r, cur
-        return r
+        multis = defaultdict(int)
+        for n in nums:
+            multis[n] = multis.get(n, 0) + n
+        prev, cur = 0, 0
+        for val in range(max(multis) + 1):
+            prev, cur = cur, max(cur, prev + multis[val])
+        return cur
