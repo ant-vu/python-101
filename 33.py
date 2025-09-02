428b627e3d79a92103c43172661e8b3b72a126d2
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-09-02 09:08:08 -0400

    feat: solution

--- 33.py
+++ 33.py
@@ -1,19 +1,18 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         # time: O(logn), space: O(1)
-        l = 0
-        r = len(nums) - 1
+        l, r = 0, len(nums) - 1
         while l <= r:
             m = l + (r - l) // 2
             if nums[m] == target:
                 return m
-            elif nums[m] >= nums[l]:
-                if nums[l] <= target <= nums[m]:
+            elif nums[l] <= nums[m]:
+                if nums[l] <= target < nums[m]:
                     r = m - 1
                 else:
                     l = m + 1
             else:
-                if nums[m] <= target <= nums[r]:
+                if nums[m] < target <= nums[r]:
                     l = m + 1
                 else:
                     r = m - 1
