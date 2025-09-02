68fc78bcd270d547194365b42a66bee73db87d62
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-09-01 21:05:20 -0400

    feat: solution

--- 127.py
+++ 127.py
@@ -1,20 +1,16 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        # time: O(n^2), space: O(n^2)
-        s = set(wordList)
-        if endWord not in s:
-            return 0
+        # time: O(n * l), space: O(n * l)
+        wordList = set(wordList)
         q = deque([(beginWord, 1)])
-        visited = {beginWord}
         while q:
-            curWord, depth = q.popleft()
-            if curWord == endWord:
-                return depth
-            for i in range(len(curWord)):
-                for j in 'abcdefghijklmnopqrstuvwxyz':
-                    if curWord[i] != j:
-                        newWord = curWord[:i] + j + curWord[i+1:]
-                        if newWord in s:
-                            s.remove(newWord)
-                            q.append((newWord, depth + 1))
+            word, length = q.popleft()
+            if word == endWord:
+                return length
+            for i in range(len(word)):
+                for c in 'abcdefghijklmnopqrstuvwxyz':
+                    nextWord = word[:i] + c + word[i+1:]
+                    if nextWord in wordList:
+                        wordList.remove(nextWord)
+                        q.append([nextWord, length + 1])
         return 0
