537004213a2eddc9f776dea72b3485439b204106
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-09-07 18:43:25 -0400

    feat: solution

--- 74.py
+++ 74.py
@@ -1,33 +1,22 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # time: O(log(m * n)), space: O(1)
-        rows = len(matrix)
-        cols = len(matrix[0])
-        l = 0
-        r = rows * cols - 1
-        while l <= r:
-            m = l + (r - l) // 2
-            row = m // cols
-            col = m % cols
-            guess = matrix[row][col]
-            if guess == target:
-                return True
-            elif guess < target:
-                l = m + 1
+        # time: O(log(mn)), space: O(1)
+        t, b = 0, len(matrix) - 1
+        while t <= b:
+            m = t + (b - t) // 2
+            if matrix[m][0] <= target <= matrix[m][-1]:
+                break
+            elif matrix[m][0] > target:
+                b = m - 1
             else:
-                r = m - 1
+                t = m + 1
+        l, r = 0, len(matrix[m]) - 1
+        while l <= r:
+            mid = l + (r - l) // 2
+            if matrix[m][mid] == target:
+                return True
+            elif matrix[m][mid] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
         return False
-
-        # time: O(log(m * n)), space: O(m * n)
-        # flatten = [x for row in matrix for x in row]
-        # l = 0
-        # r = len(flatten) - 1
-        # while l <= r:
-        #     m = l + (r - l) // 2
-        #     if flatten[m] == target:
-        #         return True
-        #     elif flatten[m] < target:
-        #         l = m + 1
-        #     else:
-        #         r = m - 1
-        # return False
